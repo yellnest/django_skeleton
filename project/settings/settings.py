5b9805a4c0d2652f526366e56a12b2f01099e6b4
Ivan-creates <63506187+Ivan-creates@users.noreply.github.com>
2022-08-06 13:59:04 +0300

    Update settings.py

--- project/settings/settings.py
+++ project/settings/settings.py
@@ -15,12 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
-STATIC_ROOT = BASE_DIR / 'static'
-MEDIA_ROOT = BASE_DIR / 'media'
-STATICFILES_DIRS = [BASE_DIR / 'static_url', ]
-
 # Application definition
 
 INSTALLED_APPS = [
